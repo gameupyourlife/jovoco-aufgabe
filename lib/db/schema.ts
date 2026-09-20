ae9643aeeace32a40fd9084359e0c0bb3a7abb6e
gameup <cedric.balzer02@gmail.com>
2026-09-20 14:35:43 +0200

    Init drizzle and better auth

---
+++ lib/db/schema.ts
@@ -0,0 +1,6 @@
+export * from "./auth-schema";
+import { integer, pgTable } from "drizzle-orm/pg-core";
+
+export const testTable = pgTable("test", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+});
